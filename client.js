4dc9789320c5723baf1363905e5db90f8d435dfc
SourceRemedy <56258014+SourceRemedy@users.noreply.github.com>
2020-10-08 13:39:38 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -5,23 +5,41 @@ console.log('Hello World!')
 let $button = document.getElementById('addTodo')
 let $textBox = document.getElementById('userInput')
 let $list = document.getElementById('list')
+let $completed = document.getElementById('completed')
 //set function as click handler
 $button.onclick = addTodo
 
 function addTodo() {
     console.log('button clicked')
-    $button.style.backgroundColor = 'red'
+    //$button.style.backgroundColor = 'red'
 
     //get text from textbox
     let newTodoText = $textBox.value
     //create new list item
     let $newTodo = document.createElement('li')
-    $newTodo.innerHTML = newTodoText
+    $newTodo.innerHTML = `${newTodoText} <button onclick ='markAsDone(event)'>Done</button> 
+    <button onclick ='deleteTodo(event)'>Delete</button>`
     //put list item in list
     $list.append($newTodo)
 
 
     
+}
+
+
+function markAsDone(event) {
+    console.log('todo done')
+    let $todo = event.target.parentElement
+    $todo.style.textDecoration = 'line-through'
+    $todo.style.color = 'lightgrey'
+    $completed.append($todo)
+    let $doneButton = event.target
+    $doneButton.remove()
+}
+
+function deleteTodo(event) {
+    console.log('todo deleted')
+    .remove()
 }
 //making h1 change to yellow
     //variable for h1
